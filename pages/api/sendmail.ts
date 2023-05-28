72abb7d3fe9a2f9aa890e6a38a400773ff22b50c
clive-cudi <clivemaina41@gmail.com>
2023-05-28 15:41:08 +0300

    added-customizations-to-email-send-api

--- pages/api/sendmail.ts
+++ pages/api/sendmail.ts
@@ -4,27 +4,72 @@ import { validateEmail } from "@/utils";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const { email, phone, message }: {email: string, phone: string, message: string} = req.body;
+        const { email, phone, message, custom }: {email: string, phone: string, message: string, custom: {
+            status: boolean,
+            sender: string,
+            senderPass: string,
+            receiver: string,
+            message: string,
+            subject: string
+        }} = req.body;
+
+        if (custom.status === true) {
+            // validate customization fields
+            const {status, message, ...requiredFields } = custom;
+
+            if (![...Object.values(requiredFields)].every(Boolean)) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Please provide all parameters for the custom send i.e Sender Email, Password and Subject"
+                });
+            }
+        }
+        const SENDER_EMAIL = `${custom.status === true ? custom.sender : process.env.SENDER_EMAIL}`;
+        const SENDER_PASS = `${custom.status === true ? custom.senderPass : process.env.SENDER_PASS}`
+        const RECEIVER_EMAIL = `${custom.status === true ? custom.receiver : process.env.RECEIVER_EMAIL}`;
         const mailTransporter = createTransport({
             service: "gmail",
             auth: {
-                user: `${process.env.SENDER_EMAIL}`,
-                pass: `${process.env.SENDER_PASS}`
+                user: `${SENDER_EMAIL}`,
+                pass: `${SENDER_PASS}`
             }
         });
 
         console.log({
-            user: `${process.env.SENDER_EMAIL}`,
-            pass: `${process.env.SENDER_PASS}`
-        })
+            user: `${SENDER_EMAIL}`,
+            pass: `${SENDER_PASS}`
+        });
+
+        if (custom.status === true) {
+            let mailDetails = {
+                from: `${SENDER_EMAIL}`,
+                to: `${RECEIVER_EMAIL}`,
+                subject: custom.subject,
+                text: `${custom.message}`
+            }
+    
+            mailTransporter.sendMail(mailDetails, (err, data) => {
+                if (err) {
+                    throw err
+                }
+    
+                console.log(data);
+                
+            })
+    
+            return res.status(200).json({
+                success: true,
+                message: "Message sent successfully!!"
+            })
+        }
 
         if (!validateEmail(email)) {
             throw new Error("Invalid Email!!")
         }
 
         let mailDetails = {
-            from: `${process.env.SENDER_EMAIL}`,
-            to: `${process.env.RECEIVER_EMAIL}`,
+            from: `${SENDER_EMAIL}`,
+            to: `${RECEIVER_EMAIL}`,
             subject: "New Message. Scottways TV.",
             text: `
                 Message from Scottways TV website.
@@ -48,7 +93,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         return res.status(200).json({
             success: true,
             message: "Message sent successfully!!"
-        })
+        });
     } catch(e) {
         console.log(e);
         return res.status(400).json({
